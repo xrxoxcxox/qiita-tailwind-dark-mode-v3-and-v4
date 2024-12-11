f30873c291093ea320ac860600d99d773a3eee82
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2024-12-11 14:11:09 +0900

    Upgrade Tailwind CSS to v4

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="flex flex-col gap-6 text-4xl bg-background h-full p-8">
-      <div className="bg-surface rounded p-6">
+      <div className="bg-surface rounded-sm p-6">
         <p className="text-text">Default color text</p>
       </div>
-      <div className="bg-surface rounded p-6">
+      <div className="bg-surface rounded-sm p-6">
         <p className="text-primary">Primary color text</p>
       </div>
     </div>
